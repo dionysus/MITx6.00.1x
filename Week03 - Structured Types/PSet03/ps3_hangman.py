a5e0c5f0b7d9cf02ccc9431f9be069637cacf87e
dionysus <dionysus.cho@gmail.com>
2018-11-13 20:46:54 -0800

    PSET03 - getGuessedWord

--- Week03 - Structured Types/PSet03/ps3_hangman.py
+++ Week03 - Structured Types/PSet03/ps3_hangman.py
@@ -63,17 +63,30 @@ def isWordGuessed(secretWord, lettersGuessed):
     
     return True
 
-
-
 def getGuessedWord(secretWord, lettersGuessed):
     '''
     secretWord: string, the word the user is guessing
     lettersGuessed: list, what letters have been guessed so far
     returns: string, comprised of letters and underscores that represents
       what letters in secretWord have been guessed so far.
+    
+    >>> secretWord = 'apple' 
+    >>> lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+    >>> print(getGuessedWord(secretWord, lettersGuessed))
+    _ pp_ e
+      
     '''
     # FILL IN YOUR CODE HERE...
-
+    
+    guessed = ''
+    
+    for letter in secretWord:
+        if letter in lettersGuessed:
+            guessed += letter
+        else:
+            guessed += '_'
+    
+    return guessed
 
 
 def getAvailableLetters(lettersGuessed):
